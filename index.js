3d3bbf10b536ee6295813ad24022db405ff14c7f
Karolis Grinkus <karolis@adaptagency.com>
2019-04-18 13:04:57 +0300

    Regenerate on file-change

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
-const dirTree = require('directory-tree');
-const fs = require('fs');
-const path = require('path');
 const argv = require('minimist')(process.argv.slice(2), {
   alias: { filename: 'f', port: 'p', listen: 'l' },
   default: { filename: 'story-coverage.html', port: 6060 },
 });
+const chokidar = require('chokidar');
+const dirTree = require('directory-tree');
+const fs = require('fs');
 const http = require('http');
+const path = require('path');
 
 let lastDepth;
 let open;
@@ -20,7 +21,7 @@ const readFileContents = (filename) => fs.readFileSync(
 const style = readFileContents('style.css');
 const script = readFileContents('script.js');
 
-const print = (obj, depth, isFile) => {
+const print = (stream, obj, depth, isFile) => {
   if (depth > lastDepth) {
     stream.write('<ul>');
     open++;
@@ -39,44 +40,10 @@ const print = (obj, depth, isFile) => {
   stream.write('</li>');
 };
 
-const stream = fs.createWriteStream(argv.filename);
-
 const pageTitle = `${path.basename(path.resolve('.'))} story coverage`;
 
-stream.once('open', fd => {
-  stream.write(`<!DOCTYPE html><title>${pageTitle}</title><style>${style}</style>`);
-  argv._.forEach(argPath => {
-    lastDepth = 0;
-    open = 0;
-    stream.write('<ul>\n');
-    walk(
-      dirTree(
-        path.resolve(argPath),
-        { extensions: /\.js$/, exclude: /\.spec\.js$/ }
-      )
-    );
-    stream.write('</ul>'.repeat((open || 0) + 1));
-    stream.write('\n');
-  });
-  stream.write(`<script>${script}</script>`);
-  stream.end();
-
-  if (argv.listen) {
-    http.createServer((req, res) => {
-      const strm = fs.createReadStream(argv.filename);
-      strm.on('error', err => {
-        res.writeHeader(404, 'Not Found');
-        res.write('404: File Not Found!');
-        return res.end();
-      });
-      res.statusCode = 200;
-      strm.pipe(res);
-    }).listen(argv.port);
-  }
-});
-
-const walk = (obj, depth = 0) => {
-  print(obj, depth, obj.type === 'file');
+const walk = (stream, obj, depth = 0) => {
+  print(stream, obj, depth, obj.type === 'file');
   if (obj.children) {
     obj.children.sort((a, b) => {
       if (a.type === 'directory' && b.type === 'file') {
@@ -94,9 +61,51 @@ const walk = (obj, depth = 0) => {
         return 1;
       }
       return 0;
-    }).forEach(child => walk(child, depth + 1));
+    }).forEach(child => walk(stream, child, depth + 1));
   }
 };
+
+const writeFile = () => {
+  const stream = fs.createWriteStream(argv.filename);
+  stream.write(`<!DOCTYPE html><title>${pageTitle}</title><style>${style}</style>`);
+  argv._.forEach(argPath => {
+    lastDepth = 0;
+    open = 0;
+    stream.write('<ul>\n');
+    walk(
+      stream,
+      dirTree(
+        path.resolve(argPath),
+        { extensions: /\.js$/, exclude: /\.spec\.js$/ }
+      )
+    );
+    stream.write('</ul>'.repeat((open || 0) + 1));
+    stream.write('\n');
+  });
+  stream.write(`<script>${script}</script>`);
+  stream.end();
+}
+
+//stream.once('open', fd => {
+  writeFile();
+
+  if (argv.listen) {
+    const watcher = chokidar.watch(argv._);
+    watcher.on('add', writeFile).on('unlink', writeFile);
+    watcher.on('addDir', path => watcher.add(path));
+    watcher.on('unlinkDir', path => watcher.unwatch(path));
+    http.createServer((req, res) => {
+      const strm = fs.createReadStream(argv.filename);
+      strm.on('error', err => {
+        res.writeHeader(404, 'Not Found');
+        res.write('404: File Not Found!');
+        return res.end();
+      });
+      res.statusCode = 200;
+      strm.pipe(res);
+    }).listen(argv.port);
+  }
+//});
 
 const cleanup = () => {
   try {
